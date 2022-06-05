e08dd254503c2c8d309603951900a8297a8c9890
MikeDB <mikedb@mikedbs-mbp.home>
2022-06-06 01:15:22 +1000

    Publish mcounter

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+let count = 0;
+
+io.on('connection', (socket) => 
+{
+ 
+  socket.on('chat message', (msg) => 
+  {
+      
+    io.emit('chat message', msg);
+    socket.emit('chat message', msg);
+    
+    if(msg == 1)
+    {
+      count ++;
+      io.emit('chat message', count);
+      socket.emit('chat message', count);
+    }
+
+  });
+
+});
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
